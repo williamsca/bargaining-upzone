a2ffa075b7e032c793c9dff3587f13acdef81984
Colin Williams <williams.colinandrew@gmail.com>
2023-06-20 10:43:55 -0400

    using openai api to extract data from pdfs

---
+++ program/Import Rezoning Data from PDFs (2023.06.19).R
@@ -0,0 +1,26 @@
+rm(list = ls())
+
+dir <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+setwd(dir)
+
+pacman::p_load(data.table, pdftools, httr, openai)
+
+file <- "ZMA201100010 Approval - County 2012-09-25.pdf"
+
+# Sys.setenv(OPENAI_API_KEY = "")
+readRenviron("~/.Renviron") # Reload .Renviron
+Sys.getenv("OPENAI_API_KEY") # Check to see that the expected key is output in your R console
+
+# API_URL <- "https://api.openai.com/v1/engines/davinci-codex/completions"
+MODEL <- "text-davinci-003"
+PROMPT <- "The following string, enclosed in triple quotations, contains an approved rezoning application. 
+Identify tax parcel ID, size, vote, and final zoning classification. Format your response as an R dictionary: 
+c(ParcelID = '', Size = '', VotesFor = , VotesAgainst = , FinalZone = ''). No context."
+
+# Read PDF as string
+pdf_text <- trimws(pdf_text(paste0(dir, "/data/Albemarle ZMAs/", file)))
+
+s <- create_completion(model = MODEL,
+                       prompt = paste0(PROMPT, "'''", pdf_text[1], "'''"),
+                       max_tokens = 500)
+
