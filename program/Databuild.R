594d3a6f88182392dd1b14448e03edd06bbea620
williamsca <williams.colinandrew@gmail.com>
2023-07-10 13:04:39 -0400

    slide pitch for leora

--- program/Databuild.R
+++ program/Databuild.R
@@ -1,61 +1,72 @@
 # Analysis of cash proffer eligibility on building permits
 # Author: Colin Williams
-# Updated: 17 March 2023
+# Updated: 10 July 2023
 
 rm(list = ls())
 
-dir <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
-setwd(dir)
-
-pacman::p_load(data.table, lubridate) 
+pacman::p_load(data.table, lubridate, here, readxl)
 
 # Import ----
-# dt.elig <- readRDS("derived/County Eligibility (2000-2022).Rds")
-dt.bp <- readRDS("derived/County Residential Building Permits by Month (2000-2021).Rds")
-dt.zhvi <- readRDS("derived/Housing Price Index (Zillow).Rds")
-dt.cp <- readRDS("derived/Cash Proffer Revenues.Rds")
+# dt_elig <- readRDS("derived/County Eligibility (2000-2022).Rds")
+dt_bp <- readRDS(
+    "derived/County Residential Building Permits by Month (2000-2021).Rds"
+)
+dt_zhvi <- readRDS("derived/Housing Price Index (Zillow).Rds")
+dt_cp <- readRDS("derived/Cash Proffer Revenues.Rds")
+dt_cpi <- read_xlsx() # TODO: defalate nominal values by CPI
 
 # Building Permits ----
-dt.bp[Name == "Bedford (Independent City)", `:=`(Name = "Bedford County", FIPS.Code.County = "019")] # Bedford City became a town on July 1, 2013. https://en.wikipedia.org/wiki/Bedford,_Virginia
-dt.bp[Name == "Clifton Forge (Independent Cit",  `:=`(Name = "Alleghany County", FIPS.Code.County = "005")] # Clifton Forge became a town in 2001. https://en.wikipedia.org/wiki/Clifton_Forge,_Virginia
+dt_bp[Name == "Bedford (Independent City)", `:=`(Name = "Bedford County", FIPS.Code.County = "019")] # Bedford City became a town on July 1, 2013. https://en.wikipedia.org/wiki/Bedford,_Virginia
+dt_bp[Name == "Clifton Forge (Independent Cit", `:=`(Name = "Alleghany County", FIPS.Code.County = "005")] # Clifton Forge became a town in 2001. https://en.wikipedia.org/wiki/Clifton_Forge,_Virginia
 
-v.cols <- grep("(Bldgs|Value|Units)", names(dt.bp), value = TRUE)
+v.cols <- grep("(Bldgs|Value|Units)", names(dt_bp), value = TRUE)
 v.cols <- grep("rep", v.cols, value =  TRUE, invert = TRUE)
-dt.bp <- dt.bp[, lapply(.SD, sum), by = .(Year4, Month, FIPS.Code.State, FIPS.Code.County, Name), .SDcols = v.cols] # combine Clifton Forge and Bedford with the surrounding county.
+dt_bp <- dt_bp[, lapply(.SD, sum), by = .(Year4, Month, FIPS.Code.State, FIPS.Code.County, Name), .SDcols = v.cols] # combine Clifton Forge and Bedford with the surrounding county.
 
-dt.bp[, Name := gsub("\\s*\\(.*", " City", Name)]
+dt_bp[, Name := gsub("\\s*\\(.*", " City", Name)]
 
-dt.bp[, FY := Year4 + fifelse(Month >= 7, 1, 0)] # The VA fiscal year runs from July 1 to June 30
+dt_bp[, FY := Year4 + fifelse(Month >= 7, 1, 0)] # The VA fiscal year runs from July 1 to June 30
 
 # Note: some proffer-collecting jurisdictions are not covered in the county building permits survey
-dt <- merge(dt.bp, dt.cp[, FIPS.Code.State := "51"], by.x = c("Name", "FY", "FIPS.Code.State"), by.y = c("Jurisdiction", "Year", "FIPS.Code.State"), 
+dt <- merge(dt_bp, dt_cp[, FIPS.Code.State := "51"], by.x = c("Name", "FY", "FIPS.Code.State"), by.y = c("Jurisdiction", "Year", "FIPS.Code.State"), 
             all.x = TRUE)
 dt[is.na(`Cash Proffer Revenue`), `Cash Proffer Revenue` := 0]
 
 dt[, Date := make_date(year = Year4, month = Month)][, FIPS := as.numeric(FIPS.Code.State)*1000 + as.numeric(FIPS.Code.County)]
-dt <- merge(dt, dt.zhvi[, .(Date, FIPS, ZHVI, RegionName)], by = c("Date", "FIPS"), all.x = TRUE)
+dt <- merge(dt, dt_zhvi[, .(Date, FIPS, ZHVI, RegionName)], by = c("Date", "FIPS"), all.x = TRUE)
 
-nrow(dt[RegionName != Name]) == 0 # TRUE --> merge of ZHVI is consistent with data
+# TRUE --> merge of ZHVI is consistent with data
+nrow(dt[RegionName != Name]) == 0
 dt$RegionName <- NULL
 
 # Treatment Intensity ----
 # Cash proffer revenues per housing value before treatment
-YEAR_RANGE <- c(2010, 2016)
+YEAR_RANGE <- c(2012, 2016)
 
-dt.VA <- dt[FIPS.Code.State %in% c("51") & FY %between% YEAR_RANGE] # filter to VA and MD (24) counties
-dt.VA[, Value := as.numeric(Value1 + Value2 + `Value3-4` + `Value5+`)]
-dt.VA <- dt.VA[, .(Value = sum(Value), `Cash Proffer Revenue` = sum(`Cash Proffer Revenue`), # UnitsLB = sum(Units1 + 2*Units2 + 3*`Units3-4` + 5*`Units5+`),
-                   Units1 = sum(Units1)), 
-                   by = .(POST = ifelse(FY >= 2017, 1, 0), FIPS.Code.State, FIPS.Code.County, Name)]
+# filter to VA counties
+dt_VA <- dt[FIPS.Code.State == "51" & FY %between% YEAR_RANGE]
 
-dt.VA[, Intensity := `Cash Proffer Revenue` / Value]
-# ggplot(dt.VA, aes(x = Intensity)) +
-#   geom_histogram() +
-#   scale_x_continuous(limits = c(-.1, .65))
+dt_VA[, Value := as.numeric(Value1 + Value2 + `Value3-4` + `Value5+`)]
 
-dt <- merge(dt, dt.VA[POST == 0, .(FIPS.Code.State, FIPS.Code.County, Name, Intensity)],
-            by = c("FIPS.Code.State", "FIPS.Code.County", "Name"), all.x = TRUE)
-dt[is.na(Intensity), Intensity := 0]
+dt_VA <- dt_VA[, .(Value = sum(Value), Units1 = sum(Units1)),
+    by = .(FIPS.Code.State, FIPS.Code.County, Name, `Cash Proffer Revenue`, FY)
+]
+
+dt_VA <- dt_VA[, .(
+    Value = sum(Value), Units1 = sum(Units1),
+    cpRev = sum(`Cash Proffer Revenue`)
+), by = .(FIPS.Code.State, FIPS.Code.County, Name)]
+
+dt_VA[, intensity12_16 := cpRev / Value]
+# ggplot(dt_VA, aes(x = intensity12_16)) +
+#     geom_histogram() +
+#     scale_x_log10()
+
+
+dt <- merge(dt, dt_VA[, .(FIPS.Code.State, FIPS.Code.County, intensity12_16)],
+    by = c("FIPS.Code.State", "FIPS.Code.County"), all.x = TRUE
+)
+dt[is.na(intensity12_16), intensity12_16 := 0]
 
 # Filter ----
 dt <- dt[!(FIPS.Code.State %in% c("02", "15"))] # exclude Alaska and Hawaii
--- program/Estimate DiD (2023.06.01).R
+++ program/Estimate DiD (2023.06.01).R
@@ -2,42 +2,54 @@
 
 rm(list = ls())
 
-pacman::p_load(data.table, stargazer, ggplot2, fixest, devtools, lubridate, here)
+pacman::p_load(
+  data.table, stargazer, ggplot2,
+  fixest, devtools, lubridate, here
+)
+
+INTENSITY_THRESHOLD <- 0.01
 
 dt <- readRDS("derived/Sample.Rds")
 
-arcsinh <- function(x) log(x + sqrt(x^2+1))
+arcsinh <- function(x) log(x + sqrt(x^2 + 1))
 
 # Treatment indicators ----
-dt[FY == 2016, everTreated := fifelse(Intensity > 0.1, 1, 0)]
+dt[FY == 2016, everTreated := fifelse(
+  intensity12_16 > INTENSITY_THRESHOLD, 1, 0
+)]
 dt[is.na(everTreated), everTreated := 0]
 dt[, everTreated := max(everTreated), by = FIPS]
 
 dt[, Post := fifelse(Date >= as.Date("2017-01-01"), 1, 0)]
 
-dt[, Units2p := Units2 + 3*`Units3-4` + 5*`Units5+`] # lower bound on multi-family units
+# lower bound on multi-family units
+dt[, Units2p := Units2 + 3 * `Units3-4` + 5 * `Units5+`]
 
-dt.qtr <- dt[, .(Units1 = sum(Units1), Units2p = sum(Units2p), ZHVI = mean(ZHVI)), 
-               by = .(FIPS, Date = quarter(Date, type = "date_first"), `Cash Proffer Revenue`, 
-                      everTreated, Post, FY, Intensity)]
-nrow(dt.qtr) == uniqueN(dt.qtr[, .(FIPS, Date)]) # TRUE --> data are unique on FIPS and quarter
+dt_qtr <- dt[, .(
+  Units1 = sum(Units1), Units2p = sum(Units2p),
+  ZHVI = mean(ZHVI)
+),
+by = .(FIPS,
+  Date = quarter(Date, type = "date_first"), `Cash Proffer Revenue`,
+  everTreated, Post, FY, intensity12_16
+)
+]
 
-# Cash Proffer Revenues ----
-dt.year <- dt[, .(nUnits1 = sum(Units1), value1 = sum(as.numeric(Value1)), nUnitsAll = sum(Units1 + Units2p)), 
-              by = .(FIPS.Code.State, FIPS.Code.County, Name, FY, `Cash Proffer Revenue`)]
-dt.year[`Cash Proffer Revenue` > 0 & nUnitsAll == 0]
-
+# TRUE --> data are unique on FIPS and quarter
+nrow(dt_qtr) == uniqueN(dt_qtr[, .(FIPS, Date)])
 
 # Summary statistics by treatment status ----
-dt.fig1 <- dt[everTreated == 1 & Units1 <= 400]
-ggplot(dt.fig1, aes(x = Units1)) + 
+dt_fig1 <- dt[everTreated == 1 & Units1 <= 400]
+ggplot(dt_fig1, aes(x = Units1)) +
   geom_histogram() +
   scale_x_continuous() +
-  labs(title = "Distribution of Monthly Building Permits: Treated Counties", 
-       subtitle = paste0(min(year(dt.fig1$Date)), " - ", max(year(dt.fig1$Date))),
+  labs(title = "Distribution of Monthly Building Permits: Treated Counties",
+       subtitle = paste0(min(year(dt_fig1$Date)), " - ",
+       max(year(dt_fig1$Date))),
        x = "Single-Unit Permits", y = "Frequency",
-       caption = paste0("Permits censored at ", max(dt.fig1$Units1), ". N = ", nrow(dt.fig1), "")) +
-  theme_light() + theme(plot.caption = element_text(hjust = 0)) # left-align caption
+       caption = paste0("Permits censored at ", max(dt_fig1$Units1),
+       ". N = ", nrow(dt_fig1), "")) +
+  theme_light() + theme(plot.caption = element_text(hjust = 0)) 
 # ggsave("", device = "pdf")
 
 # DiD ----
@@ -45,72 +57,90 @@ RHS_DID <- " ~ -1 + Post*everTreated | as.factor(Date) + as.factor(FIPS)"
 RHS_DID_CONT <- " ~ -1 + Post*Intensity | as.factor(Date) + as.factor(FIPS)"
 
 fmla.dd <- as.formula(paste0("Units1", RHS_DID))
-fmla.dd_cont <- as.formula(paste0("Units1",RHS_DID_CONT))
+fmla.dd_cont <- as.formula(paste0("Units1", RHS_DID_CONT))
 
 # * Monthly ----
 # ** Binary Treatment ----
-fepois.did <- feglm(fmla.dd, cluster = "as.factor(FIPS)", data = dt[FY %between% c(2010, 2019)],
-                    family = "quasipoisson")
+fepois.did <- feglm(fmla.dd,
+  cluster = "as.factor(FIPS)",
+  data = dt[FY %between% c(2010, 2019)],
+  family = "quasipoisson"
+)
 etable(fepois.did)
 
 # ** Continuous Treatment ----
-fepois.did <- feglm(fmla.dd_cont, cluster = "as.factor(FIPS)", data = dt[FY %between% c(2010, 2019)],
-                    family = "quasipoisson")
+fepois.did <- feglm(fmla.dd_cont,
+  cluster = "as.factor(FIPS)", data = dt[FY %between% c(2010, 2019)],
+  family = "quasipoisson"
+)
 etable(fepois.did)
+
 
 # Quarterly ----
 # (Nearly identical to monthly results)
 # ** Binary Treatment ----
-fepois.did <- feglm(fmla.dd, cluster = "as.factor(FIPS)", data = dt.qtr[FY %between% c(2010, 2019)],
-                    family = "quasipoisson")
+fepois.did <- feglm(fmla.dd,
+  cluster = "as.factor(FIPS)",
+  data = dt_qtr[FY %between% c(2010, 2019)],
+  family = "quasipoisson"
+)
 etable(fepois.did)
 
 # ** Continuous Treatment ----
-fepois.did <- feglm(fmla.dd_cont, cluster = "as.factor(FIPS)", data = dt.qtr[FY %between% c(2010, 2019)],
-                    family = "quasipoisson")
+fepois.did <- feglm(fmla.dd_cont,
+  cluster = "as.factor(FIPS)",
+  data = dt_qtr[FY %between% c(2010, 2019)],
+  family = "quasipoisson"
+)
 etable(fepois.did)
 
 
 
 # Event Study ----
 RHS_ES <- " ~ -1 + i(Date, everTreated, ref = \"2016-04-01\") | Date + FIPS"
-fmla.es <- as.formula(paste0("log(Units1)", RHS_ES))
+fmla.es <- as.formula(paste0("log(Units1 + 1)", RHS_ES))
 
 # * Monthly ----
-fepois.es <- feglm(fmla.es, cluster = "as.factor(FIPS)", data = dt[FY %between% c(2010, 2019)],
-                   family = "quasipoisson")
+# Poisson QMLE
+fepois.es <- feglm(fmla.es,
+  cluster = "as.factor(FIPS)",
+  data = dt[FY %between% c(2010, 2019)], family = "quasipoisson"
+)
 etable(fepois.es)
+iplot(fepois.es,
+  lab.fit = "simple", value.lab = "",
+  main = "Effect on single-family housing permits"
+)
 
-pdf(file = "results/eventstudy_units1.pdf")
-iplot(fepois.es, lab.fit = "simple", value.lab = "", main = "Effect on single-family housing permits")
-dev.off()
-
+# OLS
 feols.es <- feols(fmla.es,
   cluster = "as.factor(FIPS)", data = dt[FY %between% c(2010, 2019)]
 )
-iplot(feols.es, lab.fit = "simple", value.lab = "", main = "Effect on single-family housing permits")
+iplot(feols.es,
+  lab.fit = "simple",
+  value.lab = "", main = "Effect on single-family housing permits"
+)
 
-feols.zhvi <- feols(as.formula(paste0("log(ZHVI)", RHS)), 
-                 cluster = "as.factor(FIPS)", data = dt)
-etable(feols.zhvi)
-iplot(feols.zhvi, lab.fit = "simple")
 
 # Quarterly ----
+# Poisson QMLE
 fepois.es <- feglm(fmla.es,
-  cluster = "as.factor(FIPS)", data = dt.qtr[FY %between% c(2012, 2019)],
+  cluster = "as.factor(FIPS)", data = dt_qtr[FY %between% c(2012, 2019)],
   family = "quasipoisson"
 )
 etable(fepois.es)
 iplot(fepois.es, lab.fit = "simple")
+# OLS
 feols.es <- feols(fmla.es,
-  cluster = "as.factor(FIPS)", data = dt.qtr[FY %between% c(2012, 2019)]
+  cluster = "as.factor(FIPS)", data = dt_qtr[FY %between% c(2012, 2019)]
 )
 etable(feols.es)
+pdf(file = "paper/figures/eventstudy_units1.pdf")
 iplot(feols.es, lab.fit = "simple")
-
+dev.off()
 
 feols.zhvi_qtr <- feols(as.formula(paste0("log(ZHVI)", RHS)), 
-                 cluster = "as.factor(FIPS)", data = dt.qtr)
+                 cluster = "as.factor(FIPS)", data = dt_qtr)
 # etable(feols.un1_qtr)
 iplot(feols.zhvi_qtr, lab.fit = "simple")
 
@@ -119,17 +149,21 @@ iplot(feols.zhvi_qtr, lab.fit = "simple")
 devtools::install_github("synth-inference/synthdid")
 library(synthdid)
 
-dt.synthdid <- dt.qtr[Date <= as.Date("2016-06-01"), isTreated := 0] # redefine treatment indicator for synthdid
+# Define treatment indicator for synthdid
+dt.synthdid <- dt_qtr[Date <= as.Date("2016-06-01"), isTreated := 0]
 dt.synthdid[is.na(isTreated), isTreated := everTreated]
 
-dt.synthdid <- dt.synthdid[Date %between% c(as.Date("2010-01-01"), as.Date("2020-01-01")), 
-                           .(FIPS = as.factor(FIPS), Date, Units1,
-                             arcsinhUnits1 = arcsinh(Units1), 
-                             logUnits1 = log(Units1),
-                             lnZHVI = log(ZHVI), isTreated)]
+dt.synthdid <- dt.synthdid[
+  FY %between% c(2012, 2019),
+  .(
+    FIPS = as.factor(FIPS), Date, Units1,
+    arcsinhUnits1 = arcsinh(Units1),
+    logUnits1 = log(Units1 + 1), isTreated = as.logical(isTreated)
+  )
+]
 
 RunSynthDid <- function(dt, LHS) {
-  setup <- panel.matrices(dt, unit = "FIPS", time = "Date", 
+  setup <- panel.matrices(dt, unit = "FIPS", time = "Date",
                           outcome = LHS, treatment = "isTreated")
   return(synthdid_estimate(setup$Y, setup$N0, setup$T0))
 }
@@ -140,11 +174,11 @@ dt.synthdid[, nObs := sum(!is.infinite(logUnits1)), by = .(FIPS)]
 
 tau.hat <- RunSynthDid(dt.synthdid[nObs == max(nObs)], "logUnits1")
 
-plot(tau.hat, se.method = "jackknife", overlay = 0)
-ggsave("paper/figures/20230430synthdid_overlay.pdf", device = "pdf")
+plot(tau.hat, se.method = "jackknife", overlay = 1)
+ggsave("paper/figures/synthdid_overlay.pdf", device = "pdf")
 
 plot(tau.hat, se.method = "jackknife")
-ggsave("paper/figures/20230430synthdid.pdf", device = "pdf")
+ggsave("paper/figures/synthdid.pdf", device = "pdf", width = 8, height = 6)
 
 se <- sqrt(vcov(tau.hat, method = "jackknife")) # this should be a bootstrap
 sprintf("Point estimate: %1.2f", tau.hat)
