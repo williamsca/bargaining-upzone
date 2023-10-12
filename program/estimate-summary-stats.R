0e37a88175f7e648fbcd7e05ddf5935b072fd531
williamsca <williams.colinandrew@gmail.com>
2023-10-11 20:27:18 -0400

    table 1

--- program/estimate-summary-stats.R
+++ program/estimate-summary-stats.R
@@ -16,30 +16,27 @@ arcsinh <- function(x) log(x + sqrt(x^2 + 1))
 # Exclude Fairfax, Loudoun for partial exemption
 # dt <- dt[!(FIPS %in% c("51059", "51107"))]
 
-# Proffer Regressions ----
-# Proffer against density
-# TODO: in-kind value as % of total proffer value histogram
-dt_prof <- dt_app[!is.na(res_cash_proffer) &
-    !is.na(density) & isApproved == TRUE & n_units > 5]
-
-ggplot(dt_prof, aes(x = log(density),
-                    y = res_cash_proffer)) +
-    geom_point() +
-    scale_x_continuous() +
-    geom_smooth(method = "lm", formula = y ~ x + x^2)
-
-
 # Treatment indicators ----
 dt[, cp_share_local := rev_cp / rev_loc]
 
-INTENSITY_THRESHOLD <- mean(dt$cp_share_local, na.rm = TRUE)
+INTENSITY_THRESHOLD <- mean(dt[FY == 2016, cp_share_local], na.rm = TRUE)
+# INTENSITY_THRESHOLD <- quantile(dt$cp_share_local, 0.8, na.rm = TRUE)
 
-dt[FY == 2016, everTreated := fifelse(
+dt[FY == 2016, high_proffer := fifelse(
     cp_share_local > INTENSITY_THRESHOLD, 1, 0
 )]
+dt[FY == 2016, low_proffer := fifelse(
+    cp_share_local <= INTENSITY_THRESHOLD & cp_share_local > 0, 1, 0
+)]
 
-dt[is.na(everTreated), everTreated := 0]
-dt[, everTreated := max(everTreated), by = FIPS]
+dt[is.na(high_proffer), high_proffer := 0]
+dt[is.na(low_proffer), low_proffer := 0]
+dt[, high_proffer := max(high_proffer), by = FIPS]
+dt[, low_proffer := max(low_proffer), by = FIPS]
+
+dt[FY == 2016 & cp_share_local == 0, no_proffer := 1]
+dt[is.na(no_proffer), no_proffer := 0]
+dt[, no_proffer := max(no_proffer), by = FIPS]
 
 dt[, Post := fifelse(Date >= ymd("2016-07-01"), 1, 0)]
 dt[, State := substr(FIPS, 1, 2)]
@@ -47,18 +44,67 @@ dt[, State := substr(FIPS, 1, 2)]
 # Total units in multi-family buildings
 dt[, Units2p := Units2 + `Units3-4` + `Units5+`]
 
-dt_qtr <- dt[, .(
-    Units1 = sum(Units1), Units2p = sum(Units2p),
-    ZHVI = mean(ZHVI), n_units = sum(n_units)
+dt_fy <- dt[, .(
+    Units1 = sum(Units1), Units2p = sum(Units2p), Units5 = sum(`Units5+`),
+    ZHVI = mean(ZHVI), ZHVI_SFD = mean(ZHVI_SFD), n_units = sum(n_units),
+    HPI = mean(HPI)
 ),
-by = .(FIPS, PCT001001, State,
-    Date = quarter(Date, type = "date_first"), rev_cp,
-    everTreated, Post, FY
+by = .(
+    FIPS, PCT001001, State, rev_cp, rev_loc, high_proffer, Post, FY,
+    no_proffer, low_proffer
 )
 ]
+dt_fy[, `:=`(rev_loc_pc = rev_loc / PCT001001,
+    rev_cp_pc = rev_cp / PCT001001)]
+dt_fy[, nObs := .N, by = .(FIPS)]
 
 # TRUE --> data are unique on FIPS and quarter
-nrow(dt_qtr) == uniqueN(dt_qtr[, .(FIPS, Date)])
+nrow(dt_fy) == uniqueN(dt_fy[, .(FIPS, FY)])
+
+# Summary statistics ----
+dt_tab1 <- dt_fy[State == "51" & FY %between% c(2010, 2021) &
+                 nObs == max(nObs)]
+
+nrow(dt_tab1[high_proffer + no_proffer + low_proffer == 0]) == 0
+
+dt_tab1 <- dt_tab1[, .(
+    rev_loc_pc = mean(rev_loc_pc), rev_cp_pc = mean(rev_cp_pc),
+    Units1 = mean(Units1), Units2p = mean(Units2p), Units5 = mean(Units5),
+    ZHVI = mean(ZHVI, na.rm = TRUE), pop2010 = mean(PCT001001),
+    ZHVI_SFD = mean(ZHVI_SFD, na.rm = TRUE)),
+    by = .(high_proffer, no_proffer, low_proffer)
+]
+
+dt_tab1 <- dcast(melt(dt_tab1, id.vars = c(
+        "high_proffer", "low_proffer", "no_proffer"
+    ), measure.vars = c(
+        "rev_loc_pc", "rev_cp_pc", "Units1", "Units2p",
+        "Units5", "ZHVI", "ZHVI_SFD", "pop2010"
+    )), variable ~ high_proffer + no_proffer + low_proffer,
+    value.var = "value"
+)
+
+setnames(dt_tab1, c("Variable", "High Proffer", "Low Proffer",
+                    "No Proffer"))
+
+dt_tab1
+
+# TODO: print prettily
+
+# Proffer Regressions ----
+# Proffer against density
+# TODO: in-kind value as % of total proffer value histogram
+dt_prof <- dt_app[!is.na(res_cash_proffer) &
+    !is.na(density) & isApproved == TRUE & n_units > 5]
+
+ggplot(dt_prof, aes(
+    x = log(density),
+    y = res_cash_proffer
+)) +
+    geom_point() +
+    scale_x_continuous() +
+    geom_smooth(method = "lm", formula = y ~ x + x^2)
+
 
 # HPI Trends
 v_counties <- unique(dt[!is.na(n_units), FIPS])
